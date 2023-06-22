5ddf1d7b7d1f89de11b77d092bd7ffeab10f6856
Aleksdip <aleks.viunoff@gmail.com>
2023-06-22 17:51:40 +0300

    add comments to task01 version 2.0

--- src/task_01/class_room/Main.java
+++ src/task_01/class_room/Main.java
@@ -2,11 +2,12 @@ package task_01.class_room;
 
 public class Main {
     public static void main(String[] args) {
+        //create students
         Pupil pupil1 = new Pupil("Aleks", 1);
         Pupil pupil2 = new Pupil("Oleg",3);
         Pupil pupil3 = new Pupil("Volodimir", 5);
         Pupil pupil4 = new Pupil();
-
+        //add students to the array and assign them to roles
         Pupil [] students = {pupil1, pupil2, pupil3, pupil4};
         for (int i = 0; i < students.length; i++) {
             if (students[i].score < 1)
@@ -18,7 +19,7 @@ public class Main {
             else students[i] = new ExcellentPupil(students[i].name);
 
         }
-
+        //create new classroom with students
         ClassRoom classRoom = new ClassRoom(students);
         classRoom.watch();
 
